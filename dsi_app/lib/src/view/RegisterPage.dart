c4c0d42064c7bb169c6695d7f6cd046b1cc6ee76
Linaldo Neto <netoid07@gmail.com>
2025-01-02 16:04:46 -0300

    Primeiras mudanças

--- dsi_app/lib/src/view/RegisterPage.dart
+++ dsi_app/lib/src/view/RegisterPage.dart
@@ -1,12 +1,12 @@
-import 'package:dsi_app/src/services/autentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:dsi_app/src/services/autentication_service.dart';
 import 'package:dsi_app/src/core/snackbar.dart';
 
 class Registration extends StatelessWidget {
   static const routeName = '/Registration';
   Registration({super.key});
-  
+
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -25,127 +25,151 @@ class Registration extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: size.height * 0.55,
-          width: size.width,
-          child: Stack(
+      body: Stack(
+        children: [
+          Column(
             children: [
-              Column(
-                children: [
-                  const Expanded(
-                    child: Center(
-                      child: Text(
-                        'Cadastro',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 50, 201, 199),
-                            fontSize: 35),
+              // TÍTULO DA PÁGINA
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 32),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Cadastro',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 50, 201, 199),
+                        fontSize: 35,
                       ),
                     ),
                   ),
-                  // Formulario
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 32,
-                      vertical: 40,
-                    ),
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius:
-                            BorderRadius.vertical(top: Radius.circular(45))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        TextFormField(
-                          controller: _nameController,
-                          decoration: InputDecoration(
-                            hintText: 'Digite seu nome',
-                            labelText: 'Nome',
-                            prefixIcon: const Icon(Icons.person),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        TextFormField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            hintText: 'Digite seu email',
-                            labelText: 'Email',
-                            prefixIcon: const Icon(Icons.email),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        TextFormField(
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            hintText: 'Digite sua senha',
-                            labelText: 'Senha',
-                            prefixIcon: const Icon(Icons.lock),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                          ),
-                          obscureText: true,
-                        ),
-                        //BOTÃO CADASTRAR USUÁRIO
-                        Padding(
-                          padding: const EdgeInsets.only(top: 50),
-                          child: SizedBox(
-                            height: 50,
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        const Color.fromARGB(255, 50, 201, 199),
-                                    foregroundColor: Colors.white,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18),
-                                    )),
-                                onPressed: () {
-                                  _autenticationService.registerUser(
-                                    nome: _nameController.text,
-                                    email: _emailController.text,
-                                    senha: _passwordController.text).then((String? error) {
-                                    if (error != null) {
-                                      showSnackBar(context: context, message: error);
-                                    } else {
-                                      Navigator.pushNamed(context, "/Login");
-                                    }
-                                  });
-                                },
-                                child: const Text(
-                                  'Criar Conta',
-                                  style: TextStyle(fontSize: 18),
-                                )),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
 
-              // BOTÃO VOLTAR
-              Positioned(
-                left: 10,
-                top: 10,
-                child: SafeArea(
-                  child: IconButton(
-                    color: const Color.fromARGB(255, 50, 201, 199),
+              // FORMULÁRIO
+              Expanded(
+                flex: 4,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(45)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(
+                          height: 300), // Espaçamento acima dos campos
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite seu nome',
+                          labelText: 'Nome',
+                          prefixIcon:
+                              const Icon(Icons.person, color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xFFEFEFEF),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite seu email',
+                          labelText: 'Email',
+                          prefixIcon:
+                              const Icon(Icons.email, color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xFFEFEFEF),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite sua senha',
+                          labelText: 'Senha',
+                          prefixIcon:
+                              const Icon(Icons.lock, color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xFFEFEFEF),
+                        ),
+                        obscureText: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // BOTÃO "CRIAR CONTA"
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20), // Eleva o botão
+                child: SizedBox(
+                  width: size.width * 1.0,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 50, 201, 199),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                    ),
                     onPressed: () {
-                      Navigator.pushNamed(context, "/Login");
+                      _autenticationService
+                          .registerUser(
+                        nome: _nameController.text,
+                        email: _emailController.text,
+                        senha: _passwordController.text,
+                      )
+                          .then((String? error) {
+                        if (error != null) {
+                          showSnackBar(context: context, message: error);
+                        } else {
+                          Navigator.pushNamed(context, "/Login");
+                        }
+                      });
                     },
-                    icon: const Icon(Icons.arrow_back_ios),
+                    child: const Text(
+                      'Criar Conta',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
                 ),
               ),
             ],
           ),
-        ),
+
+          // BOTÃO VOLTAR
+          Positioned(
+            left: 10,
+            top: 10,
+            child: SafeArea(
+              child: IconButton(
+                color: Color.fromARGB(255, 50, 201, 199),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/Login");
+                },
+                icon: const Icon(Icons.arrow_back_ios),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
