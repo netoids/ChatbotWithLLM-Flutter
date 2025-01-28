046147eeef43ada647132faeeca4e0567da0b875
Linaldo Neto <netoid07@gmail.com>
2025-01-28 07:40:23 -0300

    Att - Apagar texto da textbox ao enviar mensagem

--- dsi_app/lib/src/view/user_chatbot_page.dart
+++ dsi_app/lib/src/view/user_chatbot_page.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 
 class EnterPage extends StatefulWidget {
   final String userName;
-  
+
   const EnterPage({super.key, required this.userName});
   static const routeName = '/EnterPage';
 
@@ -13,7 +13,7 @@ class EnterPage extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<EnterPage> {
-  final TextEditingController _userInput =TextEditingController();
+  final TextEditingController _userInput = TextEditingController();
 
   static const apiKey = "AIzaSyCDgkCO2MLuhvmjhh7cEmlJlGTqDUnO0xI";
 
@@ -21,23 +21,28 @@ class _ChatScreenState extends State<EnterPage> {
 
   final List<Message> _messages = [];
 
-  Future<void> sendMessage() async{
+  Future<void> sendMessage() async {
     final message = _userInput.text;
 
+    // Adiciona a mensagem do usuário à lista de mensagens
     setState(() {
-      _messages.add(Message(isUser: true, message: message, date: DateTime.now()));
+      _messages
+          .add(Message(isUser: true, message: message, date: DateTime.now()));
     });
+
+    // Limpa o conteúdo do TextFormField
+    _userInput.clear();
 
     final content = [Content.text(message)];
     final response = await model.generateContent(content);
 
-
+    // Adiciona a resposta do modelo à lista de mensagens
     setState(() {
-      _messages.add(Message(isUser: false, message: response.text?? "", date: DateTime.now()));
+      _messages.add(Message(
+          isUser: false, message: response.text ?? "", date: DateTime.now()));
     });
-
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,21 +55,22 @@ class _ChatScreenState extends State<EnterPage> {
         ),
       ),
       body: Container(
-        decoration: const BoxDecoration(
-          // image: DecorationImage(
-          //     colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
-          //   image: const NetworkImage('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEigDbiBM6I5Fx1Jbz-hj_mqL_KtAPlv9UsQwpthZIfFLjL-hvCmst09I-RbQsbVt5Z0QzYI_Xj1l8vkS8JrP6eUlgK89GJzbb_P-BwLhVP13PalBm8ga1hbW5pVx8bswNWCjqZj2XxTFvwQ__u4ytDKvfFi5I2W9MDtH3wFXxww19EVYkN8IzIDJLh_aw/s1920/space-soldier-ai-wallpaper-4k.webp'),
-          //   fit: BoxFit.cover
-          // )
-        ),
+        decoration: const BoxDecoration(),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Expanded(
-                child: ListView.builder(itemCount:_messages.length,itemBuilder: (context,index){
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
                   final message = _messages[index];
-                  return Messages(isUser: message.isUser, message: message.message, date: DateFormat('HH:mm').format(message.date));
-                })
+                  return Messages(
+                    isUser: message.isUser,
+                    message: message.message,
+                    date: DateFormat('HH:mm').format(message.date),
+                  );
+                },
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -79,26 +85,28 @@ class _ChatScreenState extends State<EnterPage> {
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15),
                         ),
-                        label: const Text('Enter Your Message')
+                        label: const Text('Enter Your Message'),
                       ),
                     ),
                   ),
                   const Spacer(),
                   IconButton(
                     padding: const EdgeInsets.all(12),
-                      iconSize: 30,
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(const Color.fromARGB(255, 0, 163, 160)),
-                        foregroundColor: WidgetStateProperty.all(Colors.white),
-                        shape: WidgetStateProperty.all(const CircleBorder())
-                      ),
-                      onPressed: (){
+                    iconSize: 30,
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromARGB(255, 0, 163, 160)),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all(const CircleBorder()),
+                    ),
+                    onPressed: () {
                       sendMessage();
-                      },
-                      icon: const Icon(Icons.send))
+                    },
+                    icon: const Icon(Icons.send),
+                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -106,27 +114,25 @@ class _ChatScreenState extends State<EnterPage> {
   }
 }
 
-class Message{
+class Message {
   final bool isUser;
   final String message;
   final DateTime date;
 
-  Message({ required this.isUser, required this.message, required this.date});
+  Message({required this.isUser, required this.message, required this.date});
 }
 
 class Messages extends StatelessWidget {
-
   final bool isUser;
   final String message;
   final String date;
 
-  const Messages(
-      {
-        super.key,
-        required this.isUser,
-        required this.message,
-        required this.date
-      });
+  const Messages({
+    super.key,
+    required this.isUser,
+    required this.message,
+    required this.date,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -134,29 +140,37 @@ class Messages extends StatelessWidget {
       width: double.infinity,
       padding: const EdgeInsets.all(15),
       margin: const EdgeInsets.symmetric(vertical: 15).copyWith(
-        left: isUser ? 100:10,
-        right: isUser ? 10: 100
+        left: isUser ? 100 : 10,
+        right: isUser ? 10 : 100,
       ),
       decoration: BoxDecoration(
-        color: isUser ? const Color.fromARGB(255, 0, 163, 160) : Colors.grey.shade400,
+        color: isUser
+            ? const Color.fromARGB(255, 0, 163, 160)
+            : Colors.grey.shade400,
         borderRadius: BorderRadius.only(
           topLeft: const Radius.circular(10),
-          bottomLeft: isUser ? const Radius.circular(10): Radius.zero,
+          bottomLeft: isUser ? const Radius.circular(10) : Radius.zero,
           topRight: const Radius.circular(10),
-          bottomRight: isUser ? Radius.zero : const Radius.circular(10)
-        )
+          bottomRight: isUser ? Radius.zero : const Radius.circular(10),
+        ),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             message,
-            style: TextStyle(fontSize: 16,color: isUser ? Colors.white: Colors.black),
+            style: TextStyle(
+              fontSize: 16,
+              color: isUser ? Colors.white : Colors.black,
+            ),
           ),
           Text(
             date,
-            style: TextStyle(fontSize: 10,color: isUser ? Colors.white: Colors.black,),
-          )
+            style: TextStyle(
+              fontSize: 10,
+              color: isUser ? Colors.white : Colors.black,
+            ),
+          ),
         ],
       ),
     );
